55f218667500b5fda722d28fd01e9a844556a8a3
GnarNoire <82618354+GnarNoire@users.noreply.github.com>
2021-05-26 23:12:55 -0700

    Update SB_button.py

--- SB_button.py
+++ SB_button.py
@@ -1,5 +1,5 @@
 def code():
-    for i in nuke.selectedNodes:
+    for i in nuke.selectedNodes():
         firstInput = i.input(0)
         xC = firstInput.xpos() + firstInput.screenWidth()/2
         yC = firstInput.ypos() + firstInput.screenHeight()/2    
